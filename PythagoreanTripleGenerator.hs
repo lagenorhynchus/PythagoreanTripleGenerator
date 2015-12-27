07153dd64d9e0f0d822eeeb455a533a59ead2f0e
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2015-12-27 17:08:34 +0900

    adjust indentation level

--- PythagoreanTripleGenerator.hs
+++ PythagoreanTripleGenerator.hs
@@ -9,8 +9,8 @@ pythagoreanTriples :: Integral a => [(a, a, a)]
 pythagoreanTriples =
   [(a, b, c) | c <- [1..], b <- [1..c], a <- [1..b],
     a ^ 2 + b ^ 2 == c ^ 2, coprime a b c]
-    where
-      coprime x y z = gcd x y == 1 && gcd y z == 1
+  where
+    coprime x y z = gcd x y == 1 && gcd y z == 1
 
 -- 利用例
 main :: IO ()
